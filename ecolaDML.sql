45dbf7cee610ae062815fd1c44f9de40d07f7a4a
Jair M. Diniz <jairmaiag@gmail.com>
2022-03-20 19:56:13 -0300

    Ajustes nas colunas idpessoa e idpapel

--- ecolaDML.sql
+++ ecolaDML.sql
@@ -14,10 +14,12 @@ INSERT INTO pessoa("uuid", "nome", "sobrenome","sexo", "nascimento") VALUES (uui
 INSERT INTO pessoa("uuid", "nome", "sobrenome","sexo", "nascimento") VALUES (uuid_generate_v4(), 'Melissa', 'Pitagorica', 'F','1820-10-11');
 INSERT INTO pessoa("uuid", "nome", "sobrenome", "nascimento") VALUES (uuid_generate_v4(), 'Heráclito', 'de Éfeso', '1940-06-05');
 
-INSERT INTO usuario("uuid", "senha", "apelido", "email", "idpessoa", "idpapel") VALUES (uuid_generate_v4(), 'atenas399', 'O fudador', 'socrates@gmail.com',1,1);
+INSERT INTO usuario("uuid", "senha", "apelido", "email", "pessoaid", "papelid") VALUES (uuid_generate_v4(), 'atenas399', 'O fudador', 'socrates@gmail.com',1,1);
 
 -- Consultas 
-SELECT u.uuid as uuidusu,u.apelido,u.email,p.uuid as uuidpess, p.nome,p.sobrenome,p.sexo,p.nascimento,pa.uuid as uuidpap, pa.nome AS nomepapel
+
+-- Usuários com pessoa e pepel.
+SELECT u.uuid as uuidusu,u.apelido,u.email,p.uuid as uuidpess, p.nome,p.sobrenome,p.sexo,p.nascimento,pa.uuid as uuidpapel, pa.nome AS nomepapel
 	FROM usuario u 
-	INNER JOIN pessoa p ON p.id=u.idpessoa
-	INNER JOIN papel pa ON pa.id=u.idpapel;
+	INNER JOIN pessoa p ON p.id=u.pessoaid
+	INNER JOIN papel pa ON pa.id=u.papelid;
